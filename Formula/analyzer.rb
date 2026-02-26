dff254c534debf5ef69a7dcb5948a7f29bc7bb63
Gianluigi Spagnuolo <glgspg@gmail.com>
2026-02-26 11:47:16 +0100

    Update analyzer CLI URLs and SHA256 hashes to v0.2.0

--- Formula/analyzer.rb
+++ Formula/analyzer.rb
@@ -6,20 +6,20 @@ class Analyzer < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/exein-io/analyzer-cli/releases/download/v0.1.0/analyzer-aarch64-apple-darwin.tar.gz"
-      sha256 "e6b0ee9c5162d00702d7751f22ba44d699e4b5140aad563d847d46d56067612e"
+      url "https://github.com/exein-io/analyzer-cli/releases/download/v0.2.0/analyzer-aarch64-apple-darwin.tar.gz"
+      sha256 "9635ab82af84b7a7ec9a0193a382c2fc16c8d2976b445dd8cafc6639e8c506f2"
     end
 
     on_intel do
-      url "https://github.com/exein-io/analyzer-cli/releases/download/v0.1.0/analyzer-x86_64-apple-darwin.tar.gz"
-      sha256 "96a94e4ac60197622138513268165084c3d6f71b5c1d2bd9d643a5a32424eb09"
+      url "https://github.com/exein-io/analyzer-cli/releases/download/v0.2.0/analyzer-x86_64-apple-darwin.tar.gz"
+      sha256 "ed3cd0b61ec82390dc5d4e6ba8ea6cd2e5cdd3073b9753fd7af50aaeb4769911"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/exein-io/analyzer-cli/releases/download/v0.1.0/analyzer-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "08fa618f8571af7c8d087a023dcc36d9becc0d78dacc3cca704b7560b633d1ed"
+      url "https://github.com/exein-io/analyzer-cli/releases/download/v0.2.0/analyzer-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "a966f185e40db6d719e5165873050ef4587e0619a8670a85aab012be36861a94"
     end
 
     on_intel do
