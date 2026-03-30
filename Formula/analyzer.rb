e4b814845cb4af528c74fb24aed09307a6e5cf35
Gianluigi Spagnuolo <gianluigi@exein.io>
2026-03-30 17:45:06 +0200

    Update analyzer version to 0.2.2

--- Formula/analyzer.rb
+++ Formula/analyzer.rb
@@ -1,30 +1,30 @@
 class Analyzer < Formula
   desc "CLI for Exein Analyzer - firmware and container security scanning"
   homepage "https://github.com/exein-io/analyzer-cli"
-  version "0.2.0"
+  version "0.2.2"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/exein-io/analyzer-cli/releases/download/v0.2.0/analyzer-aarch64-apple-darwin.tar.gz"
-      sha256 "9635ab82af84b7a7ec9a0193a382c2fc16c8d2976b445dd8cafc6639e8c506f2"
+      url "https://github.com/exein-io/analyzer-cli/releases/download/v0.2.2/analyzer-aarch64-apple-darwin.tar.gz"
+      sha256 "4583ffb44963bb8f69d100919743e711ea9a47c89bdbd8c08cb302c18dfd6646"
     end
 
     on_intel do
-      url "https://github.com/exein-io/analyzer-cli/releases/download/v0.2.0/analyzer-x86_64-apple-darwin.tar.gz"
-      sha256 "ed3cd0b61ec82390dc5d4e6ba8ea6cd2e5cdd3073b9753fd7af50aaeb4769911"
+      url "https://github.com/exein-io/analyzer-cli/releases/download/v0.2.2/analyzer-x86_64-apple-darwin.tar.gz"
+      sha256 "2a696dd7c6a64ea20518e61fa645b7a58804b65557eb3d9a9c267d1029d51457"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/exein-io/analyzer-cli/releases/download/v0.2.0/analyzer-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "a966f185e40db6d719e5165873050ef4587e0619a8670a85aab012be36861a94"
+      url "https://github.com/exein-io/analyzer-cli/releases/download/v0.2.2/analyzer-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "5da315f2f2f890eee6eaba6b08750089973c216668d2c2acba37cc7543bb7531"
     end
 
     on_intel do
-      url "https://github.com/exein-io/analyzer-cli/releases/download/v0.2.0/analyzer-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "37619eddede50f813a2699be86a9dd1b65f6849bc605f32a8f4fb93e08201e4b"
+      url "https://github.com/exein-io/analyzer-cli/releases/download/v0.2.2/analyzer-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "53b2cbff646fa09d170f70f25cdad59322b9562832f67d2ea380acddb9832e0a"
     end
   end
 
