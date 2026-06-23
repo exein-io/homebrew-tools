4bbbd3e3ec884b6af13887eb734a84227ed1e55c
Wojciech Brożek <wojciech@exein.io>
2026-06-23 11:10:25 +0200

    fix hash mismatch due to curl hashing 302 redirect

--- Formula/analyzer.rb
+++ Formula/analyzer.rb
@@ -7,24 +7,24 @@ class Analyzer < Formula
   on_macos do
     on_arm do
       url "https://github.com/exein-io/homebrew-tools/releases/download/analyzer-cli-0.4.1/analyzer-aarch64-apple-darwin.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "0c2cf9101e292d57e33e0bd60db0fe600e1a9acf00a76aabbc7fbda46b1ba595"
     end
 
     on_intel do
       url "https://github.com/exein-io/homebrew-tools/releases/download/analyzer-cli-0.4.1/analyzer-x86_64-apple-darwin.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "2a9e817a148eae5909bbf4a4145abca7b3096c10d41a90616717eb67b0660aa0"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/exein-io/homebrew-tools/releases/download/analyzer-cli-0.4.1/analyzer-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "db3edfb1a5e703274ac177117b0a00580116053401de063f1f4e54afaf9d0fc9"
     end
 
     on_intel do
       url "https://github.com/exein-io/homebrew-tools/releases/download/analyzer-cli-0.4.1/analyzer-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "2b9a6649ea6dea7d4f5f3cca76c7fc468dc904f26650a6d19e1e58c95f93364a"
 
     end
   end
