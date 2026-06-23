25379205ccf43603d77516d82ef2cfa1caee0c53
Wojciech Brożek <wojciech@exein.io>
2026-06-23 11:10:25 +0200

    fix hash mismatch due to curl hashing 302 redirect

--- current-hashes.sh
+++ current-hashes.sh
@@ -4,6 +4,17 @@ set -e
 
 REPO="exein-io/homebrew-tools"
 RELEASE_TAG="analyzer-cli-0.4.1"
+TMP_DIR="./tmp-dir"
+
+rm -r "$TMP_DIR" || true
+mkdir -p "$TMP_DIR"
+
+cd "$TMP_DIR"
+
+gh release download "$RELEASE_TAG"
+
+
+
 
 for target in \
   analyzer-aarch64-apple-darwin \
@@ -11,8 +22,9 @@ for target in \
   analyzer-aarch64-unknown-linux-gnu \
   analyzer-x86_64-unknown-linux-gnu
 do
+  filename="./${target}.tar.gz"
   url="https://github.com/${REPO}/releases/download/${RELEASE_TAG}/${target}.tar.gz"
-  hash=$(curl -sL "$url" | shasum -a 256 | awk '{print $1}')
+  hash=$(cat "$filename" | shasum -a 256 | awk '{print $1}')
   echo "url \"${url}\""
   echo "sha256 \"${hash}\""
 done
