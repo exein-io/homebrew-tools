1ca1be25cff6a746421972316aa8555896ea18ff
Wojciech Brożek <wojciech@exein.io>
2026-07-08 09:45:16 +0200

    hotfix: update version too

--- Formula/analyzer.rb
+++ Formula/analyzer.rb
@@ -1,7 +1,7 @@
 class Analyzer < Formula
   desc "CLI for Exein Analyzer - firmware and container security scanning"
   homepage "https://github.com/exein-io/homebrew-tools"
-  version "0.5.0"
+  version "0.5.1"
   license "Apache-2.0"
 
   on_macos do
